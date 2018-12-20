63685e8a7d1cdf4988a9524cd907e6b38af8b1aa
Кузьмичев Алексей <kual@nita.ru>
2018-12-20 11:00:11 +0300

    исправление Momento

--- H08.1-atmdepart/src/main/java/ru/otus/h8/atm/Memento.java
+++ H08.1-atmdepart/src/main/java/ru/otus/h8/atm/Memento.java
@@ -4,14 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Memento {
-    private final List<Cell> state;
+    private  final List<Cell> state;
 
     Memento(List<Cell> state) {
-        this.state = state;
-
+        if (null == state)
+            throw new IllegalStateException("Error on Momento");
+        this.state = new ArrayList<>(state.size());
+        for (Cell c : state )
+        {
+           this.state.add(c.clone());
+        }
     }
 
     List<Cell> getState() {
+        List <Cell> state = new ArrayList<>(this.state.size());
+        for(Cell c: state)
+        {
+            state.add(c.clone());
+        }
         return state;
     }
 }
